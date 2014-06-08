19ea304433bb48d7958525740cc4363a21408ea2
Kevin Yang <kevinyang72@yahoo.com>
2014-06-07 21:50:21 -0700

    Upload all files

---
+++ CardStock/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Jarloo.CardStock
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CollectData/CollectDataManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaasOne.Finance.YahooFinance;
+using MaasOne.Base;
+
+namespace CollectData
+{
+    public class CollectDataManager
+    {
+        public static void DownloadTickers()
+        {
+            
+        }
+
+        public void ScanStocks()
+        {
+
+        }
+    }
+}
