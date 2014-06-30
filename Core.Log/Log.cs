833a520d6441121a243936a1fd6360485d5e0538
Kevin Yang <kevinyang72@yahoo.com>
2014-06-30 09:56:14 -0700

    Add core.log

---
+++ Core.Log/Log.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace Core.Log
+{
+    /// <summary>
+    /// Log4Net wrapper, lifted from codecampserver (http://code.google.com/p/codecampserver/source/list)
+    /// </summary>
+    public static class Log
+    {
+        private static readonly Dictionary<Type, ILog> Loggers = new Dictionary<Type, ILog>();
+        private static bool _logInitialized;
+        private static readonly object Lock = new object();
+
+        public static string SerializeException(Exception exception)
+        {
+            return SerializeException(exception, string.Empty);
+        }
+
+        private static string SerializeException(Exception e, string exceptionMessage)
+        {
+            if (e == null) return string.Empty;
+
+            exceptionMessage = string.Format(CultureInfo.InvariantCulture,
+                                             "{0}{1}{2}\n{3}",
+                                             exceptionMessage,
+
+                                             string.IsNullOrEmpty(exceptionMessage) ? string.Empty : "\n\n",
+                                             e.Message,
+                                             e.StackTrace);
+
+            if (e.InnerException != null)
+                exceptionMessage = SerializeException(e.InnerException, exceptionMessage);
+
+            return exceptionMessage;
+        }
+
+        private static ILog GetLogger(Type source)
+        {
+            EnsureInitialized();
+            lock (Lock)
+            {
+                if (Loggers.ContainsKey(source))
+                {
+                    return Loggers[source];
+                }
+                ILog logger = LogManager.GetLogger(source);
+                Loggers.Add(source, logger);
+                return logger;
+            }
+        }
+
+        /* Log a message object */
+
+        public static void Debug(object source, string message)
+        {
+            Debug(source.GetType(), message);
+        }
+
+        public static void Debug(object source, string message, params object[] ps)
+        {
+            Debug(source.GetType(), string.Format(message, ps));
+        }
+
+        public static void Debug(Type source, string message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsDebugEnabled)
+                logger.Debug(message);
+        }
+
+        public static void Info(object source, object message)
+        {
+            Info(source.GetType(), message);
+        }
+
+        public static void Info(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsInfoEnabled)
+                logger.Info(message);
+        }
+
+        public static void Warn(object source, object message)
+        {
+            Warn(source.GetType(), message);
+        }
+
+        public static void Warn(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsWarnEnabled)
+                logger.Warn(message);
+        }
+
+        public static void Error(object source, object message)
+        {
+            Error(source.GetType(), message);
+        }
+
+        public static void Error(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsErrorEnabled)
+                logger.Error(message);
+        }
+
+        public static void Fatal(object source, object message)
+        {
+            Fatal(source.GetType(), message);
+        }
+
+        public static void Fatal(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsFatalEnabled)
+                logger.Fatal(message);
+        }
+
+        /* Log a message object and exception */
+
+        public static void Debug(object source, object message, Exception exception)
+        {
+            Debug(source.GetType(), message, exception);
+        }
+
+        public static void Debug(Type source, object message, Exception exception)
+        {
+            GetLogger(source).Debug(message, exception);
+        }
+
+        public static void Info(object source, object message, Exception exception)
+        {
+            Info(source.GetType(), message, exception);
+        }
+
+        public static void Info(Type source, object message, Exception exception)
+        {
+            GetLogger(source).Info(message, exception);
+        }
+
+        public static void Warn(object source, object message, Exception exception)
+        {
+            Warn(source.GetType(), message, exception);
+        }
+
+        public static void Warn(Type source, object message, Exception exception)
+        {
+            GetLogger(source).Warn(message, exception);
+        }
+
+        public static void Error(object source, object message, Exception exception)
+        {
+            Error(source.GetType(), message, exception);
+        }
+
+        public static void Error(Type source, object message, Exception exception)
+        {
+            GetLogger(source).Error(message, exception);
+        }
+
+        public static void Fatal(object source, object message, Exception exception)
+        {
+            Fatal(source.GetType(), message, exception);
+        }
+
+        public static void Fatal(Type source, object message, Exception exception)
+        {
+            GetLogger(source).Fatal(message, exception);
+        }
+
+        private static void Initialize()
+        {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(
+                                                               AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                               AppDomain.CurrentDomain.SetupInformation.ApplicationName + ".config")));
+            _logInitialized = true;
+
+        }
+
+        public static void EnsureInitialized()
+        {
+            if (!_logInitialized)
+            {
+                Initialize();
+
+            }
+        }
+    }
+}
---
+++ Core.Log/StaticLog.cs
@@ -0,0 +1,208 @@
+﻿//#undef USE_NAMED_HIERARCHY
+
+using System;
+using System.Globalization;
+using System.Reflection;
+using log4net.Core;
+using log4net.Util;
+using log4net.Config;
+using System.IO;
+
+namespace Core.Log
+{
+    /// <summary>
+    /// Provides static methods for logging.
+    /// </summary>
+    /// <remarks>
+    /// This is a light wrapper around log4net. It provides set of static logging methods that can
+    /// be called anywhere without prior initialization. The log output targets and formatting can
+    /// be configured in the standard log4net fashion.
+    /// </remarks>
+    public static class StaticLog
+    {
+#if !USE_NAMED_HIERARCHY
+        /// <summary>
+        /// The singleton logger instance used for all logging output.
+        /// </summary>
+        private static readonly ILogger Logger = InitializeLogger();
+#endif
+
+        /// <summary>
+        /// Logs a formatted message string with the <see cref="Level.Trace"/> level.
+        /// </summary>
+        /// <param name="format">The message format string to log.</param>
+        /// <param name="args">An array containing the message format parameters.</param>
+        public static void Trace(string format, params object[] args)
+        {
+            LogMessage(Level.Trace, format, args);
+        }
+
+        /// <summary>
+        /// Logs a message string with the <see cref="Level.Trace"/> level.
+        /// </summary>
+        /// <param name="message">The message string to log.</param>
+        public static void Trace(string message)
+        {
+            LogMessage(Level.Trace, message);
+        }
+
+        /// <summary>
+        /// Logs a formatted message string with the <see cref="Level.Debug"/> level.
+        /// </summary>
+        /// <param name="format">The message format string to log.</param>
+        /// <param name="args">An array containing the message format parameters.</param>
+        public static void Debug(string format, params object[] args)
+        {
+            LogMessage(Level.Debug, format, args);
+        }
+
+        /// <summary>
+        /// Logs a message string with the <see cref="Level.Debug"/> level.
+        /// </summary>
+        /// <param name="message">The message string to log.</param>
+        public static void Debug(string message)
+        {
+            LogMessage(Level.Debug, message);
+        }
+
+        /// <summary>
+        /// Logs a formatted message string with the <see cref="Level.Info"/> level.
+        /// </summary>
+        /// <param name="format">The message format string to log.</param>
+        /// <param name="args">An array containing the message format parameters.</param>
+        public static void Info(string format, params object[] args)
+        {
+            LogMessage(Level.Info, format, args);
+        }
+
+        /// <summary>
+        /// Logs a message string with the <see cref="Level.Info"/> level.
+        /// </summary>
+        /// <param name="message">The message string to log.</param>
+        public static void Info(string message)
+        {
+            LogMessage(Level.Info, message);
+        }
+
+        /// <summary>
+        /// Logs a formatted message string with the <see cref="Level.Warn"/> level.
+        /// </summary>
+        /// <param name="format">The message format string to log.</param>
+        /// <param name="args">An array containing the message format parameters.</param>
+        public static void Warn(string format, params object[] args)
+        {
+            LogMessage(Level.Warn, format, args);
+        }
+
+        /// <summary>
+        /// Logs a message string with the <see cref="Level.Warn"/> level.
+        /// </summary>
+        /// <param name="message">The message string to log.</param>
+        public static void Warn(string message)
+        {
+            LogMessage(Level.Warn, message);
+        }
+
+        /// <summary>
+        /// Logs an exception with the <see cref="Level.Error"/> level.
+        /// </summary>
+        /// <param name="ex">The exception to log, including its stack trace.</param>
+        public static void Error(Exception ex)
+        {
+            LogException(Level.Error, "Error.", ex);
+        }
+
+        public static void Error(string message, Exception ex)
+        {
+            LogException(Level.Error, message, ex);
+        }
+
+        public static void Error(string message)
+        {
+            LogMessage(Level.Error, message);
+        }
+        /// <summary>
+        /// Logs an exception with the <see cref="Level.Fatal"/> level.
+        /// </summary>
+        /// <param name="ex">The exception to log, including its stack trace.</param>
+        public static void Fatal(Exception ex)
+        {
+            LogException(Level.Fatal, "Fatal error.", ex);
+        }
+
+        public static void Fatal(string message, Exception ex)
+        {
+            LogException(Level.Fatal, message, ex);
+        }
+
+        /// <summary>
+        /// Logs a formatted message string with the specified <see cref="Level"/>.
+        /// </summary>
+        private static void LogMessage(Level level, string format, params object[] args)
+        {
+            var logger = GetLoggerInstance();
+            if (logger.IsEnabledFor(level))
+            {
+                var message = new SystemStringFormat(CultureInfo.InvariantCulture, format, args);
+                logger.Log(typeof(Log), level, message, null);
+            }
+        }
+
+        /// <summary>
+        /// Logs a message string with the specified <see cref="Level"/>.
+        /// </summary>
+        private static void LogMessage(Level level, string message)
+        {
+            var logger = GetLoggerInstance();
+            if (logger.IsEnabledFor(level))
+            {
+                logger.Log(typeof(Log), level, message, null);
+            }
+        }
+
+        /// <summary>
+        /// Logs an exception with the specified <see cref="Level"/>.
+        /// </summary>
+        private static void LogException(Level level, string message, Exception ex)
+        {
+            var logger = GetLoggerInstance();
+            if (logger.IsEnabledFor(level))
+            {
+                logger.Log(typeof(Log), level, message, ex);
+            }
+        }
+
+#if !USE_NAMED_HIERARCHY
+        /// <summary>
+        /// Initializes the singleton logger instance.
+        /// </summary>
+        private static ILogger InitializeLogger()
+        {
+            const string name = "*";
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(
+                                                              AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                              AppDomain.CurrentDomain.SetupInformation.ApplicationName + ".config")));
+            return LoggerManager.GetLogger(Assembly.GetCallingAssembly(), name);
+        }
+
+        /// <summary>
+        /// Retrieves the singleton logger instance.
+        /// </summary>
+        private static ILogger GetLoggerInstance()
+        {
+            return Logger;
+        }
+#else
+        /// <summary>
+        /// Retrieves a logger based on the current state of the stack frame. 
+        /// </summary>
+        private static ILogger GetLoggerInstance()
+        {
+            var stackFrame = new System.Diagnostics.StackFrame(3);
+            var method = stackFrame.GetMethod();
+            var type = method.ReflectedType;
+            return LoggerManager.GetLogger(Assembly.GetCallingAssembly(), type);
+        }
+#endif
+    }
+}
