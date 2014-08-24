6ca0071d28394b57ffa67cd3a698ed6b5bd7d45a
Kevin Yang <kevinyang72@yahoo.com>
2014-08-24 10:45:37 -0700

    My Fda update

--- TheFishNet/TheFishNet/Controllers/FdaCalendarsController.cs
+++ TheFishNet/TheFishNet/Controllers/FdaCalendarsController.cs
@@ -15,9 +15,19 @@ namespace TheFishNet.Controllers
         private TheFishEntities db = new TheFishEntities();
 
         // GET: FdaCalendars
-        public ActionResult Index()
+        public ActionResult Index(long? id)
         {
-            return View(db.FdaCalendars.ToList());
+            if (id == null)
+            {
+                return View(db.FdaCalendars.ToList());
+            }
+            else
+            {
+                var nowDate = DateTime.Now.AddDays(-1);
+                return View(db.FdaCalendars.Where(x => x.Price < 10
+                    && (x.Type.Equals("PDUFA", StringComparison.OrdinalIgnoreCase) || x.Type.Equals("Advisory Committee", StringComparison.OrdinalIgnoreCase))
+                    && x.CatalystDate > nowDate).ToList());
+            }
         }
 
         // GET: FdaCalendars/Details/5
